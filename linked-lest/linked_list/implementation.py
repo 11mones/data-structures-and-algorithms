96b632861b53858811291964f548abdaacddb50a
11mones <mones_leader@hotmail.com>
2023-05-15 21:47:52 +0000

    Now DDonEE

--- linked-lest/linked_list/implementation.py
+++ linked-lest/linked_list/implementation.py
@@ -61,24 +61,16 @@ class linked_list :
 
 
     
-    # def insert_before (self , target , value):
-    #     new_node = node(value)
-    #     temp = self.head
-    #     c =0
-    #     while temp.data != target :
-    #         temp = temp.next
-    #         c+=1
-    #     print(c)
-    #     temp1 = self.head
-    #     c1 =0
-    #     while temp1.next is not None :
-    #         temp1 = temp1.next
-    #         c1+=1
-    #         if(c1 == (c-1)) :
-    #             temp1.next = new_node
-    #             new_node.next = temp1.next.next
+
     def insert_before(self, target, value):
         new_node = node(value)
+        if self.head is None:
+            return
+        if self.head.data == target:
+            new_node.next = self.head
+            self.head = new_node
+            return
+
         current = self.head
         while current.next is not None:
             if current.next.data == target:
@@ -86,17 +78,24 @@ class linked_list :
                 current.next = new_node
                 return
             current = current.next
-            
+
+   
+
+
+    def insert_after(self, target, value):
+        new_node = node(value)
+        current = self.head
+        while current.next is not None:
+            if current.next.data == target:
+                current = current.next
+                new_node.next = current.next
+                current.next = new_node
+                return
+            current = current.next
             
         
 
-ll = linked_list()
-ll.append(5)
-ll.append(6)
-ll.append(7)
-ll.append(8)
-    # now i have : head -> {5} -> {6} -> {7} -> {8} -> X
-ll.insert_before(7,90)
+
 
  
 
